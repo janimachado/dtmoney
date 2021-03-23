93401ed857aba3caea5dfb8730f436c3e953a3c9
janimachado <janicota@hotmail.com>
2021-03-23 14:59:29 -0300

    final

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,8 +4,8 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImage from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { api } from "../../services/api";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
+
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -15,7 +15,7 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
 
     const [type, setType] = useState('deposit');
     const [title, setTitle] = useState('');
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
-import { api } from './services/api';
+import { createContext, ReactNode, useEffect, useState, useContext } from 'react'
+import { api } from '../services/api';
 
 interface Transaction {
     id: number;
@@ -21,7 +21,7 @@ interface TransactionsContextData {
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -53,4 +53,11 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         </TransactionsContext.Provider>
     );
 
+}
+
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
 }
